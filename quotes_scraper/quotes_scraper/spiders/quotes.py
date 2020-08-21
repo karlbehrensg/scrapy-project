db0853aee29b6d340d83bba421cb47de5346d72a
Karl Behrens <karlbehrensg@gmail.com>
2020-08-20 21:15:54 -0400

    Usando XPath para extraer datos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,5 +1,10 @@
 import scrapy
 
+# Titulo = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+
+
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -8,7 +13,25 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         print('*' * 10)
+        print('\n\n\n')
+
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
         print('\n\n')
-        print(response.status, response.headers)
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print('Citas: ')
+        for quote in quotes:
+            print(f'- {quote}')
         print('\n\n')
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+
+        print('Top ten tags: ')
+        for tag in top_ten_tags:
+            print(f'- {tag}')
+
+        #print(response.status, response.headers)
+
+        print('\n\n\n')
         print('*' * 10)
